093e1dd35574a904201419eaa88e37d293322aba
Nathan Gang <nathan.gang@limitbreak.com>
2023-10-25 09:16:40 -0600

    sepolia deploy scripts

---
+++ script/sepolia/DeployCPortEncoder.s.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
+
+import "forge-std/Script.sol";
+import "forge-std/console2.sol";
+import "src/cPortEncoder.sol";
+
+contract DeployCPortEncoder is Script {
+    function run() public {
+        bytes32 saltValue = bytes32(vm.envUint("SALT_CPORT_ENCODER"));
+        uint256 deployerPrivateKey = vm.envUint("DEPLOYER_KEY");
+        vm.startBroadcast(deployerPrivateKey);
+
+        address cPortEncoderAddress = address(new cPortEncoder{salt: saltValue}());
+
+        vm.stopBroadcast();
+
+        console.log("cPortEncoder: ", cPortEncoderAddress);
+    }
+}
